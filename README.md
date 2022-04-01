3020523cd6c42dd8d2a9ef043e0720c84eae4ac2
Keerti Chaudhary <17keerti@gmail.com>
2022-03-31 22:38:53 -0700

    Implementing generate password method

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# password-generator-29thmarch
+# Password Generator using Javascript
+It generates a random password based on user-selected criteria. It gives several prompts to users to select options. Once all prompts are answered, the user will be given a password of their choice.
+
+## Technology used
+--HTML
+--CSS
+--Javascript
+
+## Screenshots
+
+## Learnings
+
+
+## Deployment URL
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getRandomChar(str) {
+  var rnum = Math.floor(Math.random() * str.length);
+  return str[rnum];
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
 function getUserInput() {
   var valid1 = true;
-  var passwordLength1 = prompt("Choose the length of your password");
+  var passwordLength1 = prompt("Choose the length of your password between 8- 128.");
+  console.log(passwordLength1);
   if (passwordLength1 < 8 || passwordLength1 > 128) {
     valid1 = false;
     alert("You must choose password length between 8 and 128");
-  } else {
+  } else if (isNaN(passwordLength1)){
+    alert("Please choose a number.");
+  }
+   else {
     var lowerCase1 = confirm("Do you want to include lowercase in your password ?");
     var upperCase1 = confirm("Do you want to include Uppercase in your password ?");
     var specialCharacters1 = confirm("Do you want to include special characters in your password");
@@ -17,10 +31,7 @@ function getUserInput() {
       valid1 = false;
       alert("You must select at least one character type");
     }
-    console.log("Password length: " + passwordLength1 + " lowerCase: " + lowerCase1 + " upperCase: " + upperCase1 + " specialCharacters: " + specialCharacters1 + " numeric: " + numeric1);
-
   }
-
 
   let response = {
     passwordLength: passwordLength1,
@@ -31,11 +42,45 @@ function getUserInput() {
     valid: valid1
   }
   return response;
+}
 
+
+function generatePassword(userResponse) {
+
+  var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specialChars = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
+  var allCharSetArray = [];
+
+  if (userResponse.lowerCase === true) {
+    allCharSetArray.push(lowerCaseChars);
+  }
+  if (userResponse.upperCase == true) {
+    allCharSetArray.push(upperCaseChars);
+  }
+  if (userResponse.specialCharacters === true) {
+    allCharSetArray.push(specialChars);
+  }
+  if (userResponse.numeric === true) {
+    allCharSetArray.push(numbers);
+  }
+
+  var finalResult = '';
+  for (var i = 0; i < userResponse.passwordLength; i++) {
+
+    randomArrayIndex = getRandomInt(allCharSetArray.length);
+    var randomCharSet = allCharSetArray[randomArrayIndex];
+
+    randomChar = getRandomChar(randomCharSet);
+    // console.log(randomChar);
+    finalResult = finalResult + randomChar;
+  }
+  return finalResult ;
 }
-function generatePassword(arg1) {
-  return "samplepassword";
-}
+
+
+
 
 // Write password to the #password input
 
